aff47664b2b65e114ea62270f228a3cb094ed500
andrei850 <56795644+andrei850@users.noreply.github.com>
2019-10-20 19:57:53 -0500

    Add files via upload

---
+++ treelist.js
@@ -0,0 +1,23 @@
+﻿
+$(document).ready(function () {
+
+    $("#treelist").kendoTreeList({
+        dataSource: new kendo.data.TreeListDataSource({
+            transport: {
+                read: {
+                    url: "/Home/CreateList",
+                    dataType: "json"
+                }
+            }
+        }),
+        columns: [
+            { field: "CountID" },
+            { field: "Reason" }
+        ]
+    });
+    
+
+    // Get a reference to the kendo.ui.TreeList instance.
+    var treelist = $("#treelist").data("kendoTreeList");
+
+});
