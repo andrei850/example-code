09932fb4cf78cc0d51087d2c28f0b9252e7db790
andrei850 <56795644+andrei850@users.noreply.github.com>
2019-10-20 19:56:06 -0500

    Add files via upload

---
+++ Reason.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.Models
+{
+    public class ReasonData
+    {
+        public string ID { get; set; }
+        public string ParentId { get; set; }
+        public string CountID { get; set; }
+        public string Reason { get; set; }
+    }
+}
