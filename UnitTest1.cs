dba6144c82db17982f3b60423d4ddb1306187172
andrei850 <56795644+andrei850@users.noreply.github.com>
2019-10-20 19:54:19 -0500

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Example.Controllers;
+
+namespace Example.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ListData dl = new ListData();
+
+            var lst = dl.GetDataList();
+            Assert.AreEqual(3, lst.Count);
+
+        }
+    }
+}
